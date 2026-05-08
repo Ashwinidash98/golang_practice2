f7062d4e0d9336808c897033871d00120973c54e
Ashwini Dash <ashwinidash98@gmail.com>
2026-05-08 22:28:54 +0530

    initial commit

---
+++ cmd/web/main.go
@@ -0,0 +1,143 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"sync"
+	"time"
+
+	"github.com/alexedwards/scs/redisstore"
+	"github.com/alexedwards/scs/v2"
+	"github.com/gomodule/redigo/redis"
+	_ "github.com/jackc/pgconn"
+	_ "github.com/jackc/pgx/v4"
+	_ "github.com/jackc/pgx/v4/stdlib"
+)
+
+const webPort = "80"
+
+func main() {
+	//Connect to database
+	db := initDB()
+	db.Ping()
+
+	//create sessions
+	session := initSession()
+
+	//create loggers
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	//Create channels
+
+	//create waitgroup
+	wg := sync.WaitGroup{}
+
+	//setup the application config
+	app := Config{
+		Session:  session,
+		DB:       db,
+		InfoLog:  infoLog,
+		ErrorLog: errorLog,
+		Wait:     &wg,
+	}
+
+	//set up mail
+
+	//listen for web connections
+	app.serve()
+}
+
+func (app *Config) serve() {
+	//start http server
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%s", webPort),
+		Handler: app.routes(),
+	}
+
+	app.InfoLog.Println("starting web server...")
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Panic(err)
+	}
+}
+
+// initDB connects to Postgres and returns a pool of connections
+func initDB() *sql.DB {
+	conn := connectToDB()
+	if conn == nil {
+		log.Panic("can't connect to DB")
+	}
+	return conn
+}
+
+func connectToDB() *sql.DB {
+	count := 0
+
+	//dsn := os.Getenv("DSN")
+	dsn := "host=localhost port=5432 user=postgres password=password dbname=concurrency sslmode=disable"
+
+	for {
+		connection, err := openDB(dsn)
+		if err != nil {
+			log.Println("Postgres not yet ready...")
+		} else {
+			log.Println("connected to database")
+			return connection
+		}
+
+		if count > 5 {
+			return nil
+		}
+
+		log.Println("backing of for 1 sec")
+		time.Sleep(1 * time.Second)
+		count++
+
+		fmt.Println(dsn)
+
+		continue
+	}
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("pgx", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
+
+func initSession() *scs.SessionManager {
+
+	//set up session
+	session := scs.New()
+	session.Store = redisstore.New(initRedis())
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = true
+
+	return session
+}
+
+func initRedis() *redis.Pool {
+
+	redisPool := &redis.Pool{
+
+		MaxIdle: 10,
+		Dial: func() (redis.Conn, error) {
+			return redis.Dial("tcp", os.Getenv("REDIS"))
+		},
+	}
+	return redisPool
+}
